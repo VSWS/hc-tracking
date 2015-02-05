e4e83fc75a46453d4bc9cdbaaa8facdd3c42fb04
NgaNguyenDuy <kyo1508@gmail.com>
2015-02-06 05:26:08 +0700

    update server

--- udp.js
+++ udp.js
@@ -2,8 +2,20 @@
  * Created by tungtouch on 2/5/15.
  */
 var dgram = require('dgram');
+var buf;
 
 var client = dgram.createSocket('udp4', function(data){
-    console.log("UDP Data: ", data);
+    buf = new Buffer(data, "hex");
+    console.log("UDP Data: ", buf);
 });
     client.bind(3333);
+
+
+
+
+
+
+
+
+
+
