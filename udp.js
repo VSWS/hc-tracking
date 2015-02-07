231fa1e1823e1970e90ea887c41edb81e7d24642
tungtouch <tungnguyen@vsoft.vn>
2015-02-07 15:51:08 +0700

    udp

--- udp.js
+++ udp.js
@@ -14,21 +14,29 @@ var ports = [4000, 4001, 4002, 3333, 4003, 4004, 4005, 4006, 4007, 4008, 4009, 4
 
 
 for(var i=1; i < ports.length; i++){
-    var server = dgram.createSocket("udp4");
+
+    var client = dgram.createSocket('udp4', function(data){
+        console.log("")
+        console.log("[1. Data Raw]: ".green, data);
+        console.log("[2. JSON Data]: ".yellow, JSON.stringify(data));
+        //console.log("[3. Decoder:]".blue, typeof data, data.toString('utf8'));
+        console.log("-------------------------------------------------");
+
+    })
 
     console.log("Start listen port ",i,":",ports[i]);
 
-    server.on("error", function (err) {
+    client.on("error", function (err) {
         console.log("Server error:\n".red + err.stack);
         server.close();
     });
 
-    server.on("message", function (msg, rinfo) {
+    client.on("message", function (msg, rinfo) {
         console.log("Server got: ".yellow + msg + " from " +
         rinfo.address + ":" + rinfo.port);
     });
 
-    server.on("listening", function () {
+    client.on("listening", function () {
         var address = server.address();
         console.log("Server listening ".blue +
         address.address + ":" + address.port);
