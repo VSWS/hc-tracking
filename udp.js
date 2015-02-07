4b2a06663e98f48a29cdc2b6a88adce2d89ad092
tungtouch <tungnguyen@vsoft.vn>
2015-02-07 15:37:12 +0700

    tcp

--- udp.js
+++ udp.js
@@ -11,25 +11,26 @@ var colors = require('colors');
 var conv = require('binstring');
 var ports = [4000, 4001, 4002, 4003, 4004, 4005, 4006, 4007, 4008, 4009, 4010];
 
-var client = dgram.createSocket('udp4', function(data){
-    console.log("[1. Data Raw]: ".green, data);
-    console.log("[2. JSON Data]: ".yellow, JSON.stringify(data));
-    //console.log("[3. Decoder:]".blue, typeof data, data.toString('utf8'));
-    console.log("-------------------------------------------------");
-    //console.log("2. New Buffer:");
-    var arrData = JSON.stringify(data);
-    var buf = new Buffer(arrData);
-    //console.log('- BinString 1: ', conv(arrData, { out:'hex' }));
-    //console.log('- BinString 2: ', conv(arrData, { out:'utf8' }));
-    //console.log('- Buffer: ', typeof buf, buf);
-    //console.log('- Decoder: ', buf.toString('utf8'));
 
-
-
-    //console.log( "Decode: " + buff.toString('hex'));
-});
 for(var i=1; i < ports.length; i++){
     console.log("Start listen port ",i,":",ports[i]);
+    var client = dgram.createSocket('udp4', function(data){
+        console.log("[1. Data Raw]: ".green, data);
+        console.log("[2. JSON Data]: ".yellow, JSON.stringify(data));
+        //console.log("[3. Decoder:]".blue, typeof data, data.toString('utf8'));
+        console.log("-------------------------------------------------");
+        //console.log("2. New Buffer:");
+        var arrData = JSON.stringify(data);
+        var buf = new Buffer(arrData);
+        //console.log('- BinString 1: ', conv(arrData, { out:'hex' }));
+        //console.log('- BinString 2: ', conv(arrData, { out:'utf8' }));
+        //console.log('- Buffer: ', typeof buf, buf);
+        //console.log('- Decoder: ', buf.toString('utf8'));
+
+
+
+        //console.log( "Decode: " + buff.toString('hex'));
+    });
     client.bind(ports[i]);
 }
     //client.bind(3333);
