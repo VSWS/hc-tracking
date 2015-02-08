c73c42685c7b1c4f06ce55953440e32f3138d03a
tungtouch <tungnguyen@vsoft.vn>
2015-02-09 01:02:55 +0700

    change port 3000

--- benchmark.js
+++ benchmark.js
@@ -2,9 +2,18 @@
  * Created by tungtouch on 2/9/15.
  */
 var dgram = require('dgram');
-var message = new Buffer("Some bytes");
+
 var client = dgram.createSocket("udp4");
-client.send(message, 0, message.length, 4444, "128.199.126.250", function(err) {
-    console.log("OK");
-    client.close();
-});
+
+var max = 10000;
+
+for (var i = 0; i < max; i++) {
+    var message = new Buffer("Some bytes:"+i);
+    client.send(message, 0, message.length, 4444, "128.199.126.250", function (err) {
+        console.log("OK: ", i);
+        if(err){
+            console.log(err);
+        }
+        client.close();
+    });
+}
