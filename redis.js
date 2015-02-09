6e781cb8de23a0c365830339b51049b532d7e6d8
tungtouch <tungnguyen@vsoft.vn>
2015-02-09 20:10:50 +0700

    change port 3000

--- redis.js
+++ redis.js
@@ -8,13 +8,20 @@ var rClient = redis.createClient();
 var message = new Buffer("Some bytes hello world bo bo bo world HEHEHEHE ahhaha hohoho hehe ban kinh dai bakbakabk efefe");
 var async = require('async');
 
-var max = 20000000;
-
+var max = 200000;
+var arr = [];
 
 for (var i = 0; i < max; i++) {
-    console.log("Insert Data:", i);
-    rClient.hset("raw", "data"+i, message, redis.print);
+    arr.push(i);
 }
+console.log("Array:", arr);
+var q = async.queue(function(index, cb){
+    setTimeout(function () {
+        rClient.hset("raw", "data"+index, message, redis.print);
+        console.log("Data", index);
+    }, 10);
+});
 
+q.push(arr);
 
 console.log("Starting Benchmark!");
