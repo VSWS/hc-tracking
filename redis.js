0c69ad56ff2691c8527fba5b87b7e79def3eb955
tungtouch <tungnguyen@vsoft.vn>
2015-02-09 20:32:03 +0700

    change port 3000

--- redis.js
+++ redis.js
@@ -3,7 +3,7 @@
  * Created by tungtouch on 2/9/15.
  */
 
-var http = require("http"), server;
+var http = require("http"), server, i;
 var message = new Buffer("Some bytes hello world bo bo bo world HEHEHEHE ahhaha hohoho hehe ban kinh dai bakbakabk efefe");
 var redis_client = require("redis").createClient();
 var r = 0;
@@ -13,8 +13,9 @@ server = http.createServer(function (request, response) {
         "Content-Type": "text/plain"
     });
     console.log("Insert data", r);
-    redis_client.hset("raw", "data"+r, message, redis_client.print);
-
+    for(i=0; i<2000; i++){
+        redis_client.hset("raw", "data"+r, message, redis_client.print);
+    }
 }).listen(6666);
 
 server.on('error', function(err){
