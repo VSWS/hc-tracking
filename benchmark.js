ebdac4ec0f577b157d3dc1f4e1727c17cd603019
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 05:52:30 +0700

    Update UDPefef

--- benchmark.js
+++ benchmark.js
@@ -39,7 +39,7 @@ var q = async.queue(function(index, cb){
 
 if (cluster.isMaster) {
     // Fork workers.
-    for (var i = 0; i < numCPUs; i++) {
+    for (var e = 0; e < numCPUs; e++) {
         cluster.fork();
     }
 
@@ -50,5 +50,5 @@ if (cluster.isMaster) {
     // Workers can share any TCP connection
     // In this case its a HTTP server
     q.push(arr);
-}
+};
 console.log("Starting Benchmark!");
