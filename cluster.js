6eb2f0d3992a1ad656619126810e07ab0e350118
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 05:55:55 +0700

    Update UDPefef

--- cluster.js
+++ cluster.js
@@ -6,14 +6,29 @@ var http = require('http');
 var numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-    // Fork workers.
+    // Keep track of http requests
+    var numReqs = 0;
+    setInterval(function() {
+        console.log("numReqs =", numReqs);
+    }, 1000);
+
+    // Count requestes
+    function messageHandler(msg) {
+        if (msg.cmd && msg.cmd == 'notifyRequest') {
+            numReqs += 1;
+        }
+    }
+
+    // Start workers and listen for messages containing notifyRequest
+    //var numCPUs = require('os').cpus().length;
     for (var i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
 
-    cluster.on('exit', function(worker, code, signal) {
-        console.log('worker ' + worker.process.pid + ' died');
+    Object.keys(cluster.workers).forEach(function(id) {
+        cluster.workers[id].on('message', messageHandler);
     });
+
 } else {
     // Workers can share any TCP connection
     // In this case its a HTTP server
