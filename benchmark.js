16bba952b65470a92cf2c43e6ff80c8ecd9e06a8
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 05:51:00 +0700

    Update UDPefef

--- benchmark.js
+++ benchmark.js
@@ -20,8 +20,6 @@ for (var i = 0; i < max; i++) {
     arr.push(i);
 }
 
-console.log("Num CPU:", numCPUs);
-
 var q = async.queue(function(index, cb){
     setTimeout(function () {
         client.send(message, 0, message.length, 4444, "128.199.126.250", function (err) {
@@ -41,7 +39,7 @@ var q = async.queue(function(index, cb){
 
 if (cluster.isMaster) {
     // Fork workers.
-    for (var i = 0; i < 80; i++) {
+    for (var i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
 
