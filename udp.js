c42abfb00a4378ea55e89088684373be15c3597b
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 15:15:22 +0700

    Update UDPefef

--- udp.js
+++ udp.js
@@ -4,6 +4,7 @@ var rClient = redis.createClient();
 var dgram = require("dgram");
 
 var FIFO = new d();
+var index =0;
 
 fetcher();
 
@@ -13,20 +14,20 @@ udpserver.on('error', function (err) {
 })
 udpserver.on("message",
     function (msg, rinfo) {
+        index++;
         FIFO.push(msg.toString());
+        console.log("Success:", index);
     }
 );
 
 udpserver.bind(4444);
 
-var index =0;
+
 
 function fetcher () {
     while (FIFO.length > 0)
     {
         var msg = FIFO.shift();
-        index++;
         rClient.hset("raw", "data"+index, msg);
-        console.log("Success: ", index);
     }
 }
