9ecc688c356e477aae14adc1f0da73a4b725584d
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 14:16:52 +0700

    Update UDPefef

--- udp.js
+++ udp.js
@@ -7,11 +7,12 @@
 * */
 
 var dgram = require("dgram");
-//  var colors = require('colors');
+var colors = require('colors');
+var Dequeue = require('dequeue')
 var async = require('async');
 var redis = require("redis");
 var rClient = redis.createClient();
-
+var FIFO = new Dequeue();
 //var ports = [4000, 4001, 4002, 3333, 4003, 4004, 4005, 4006, 4007, 4008, 4009, 4010, 3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009, 3010, 3011];
 var ports = [4444];
 var r = 0;
@@ -23,11 +24,8 @@ rClient.on("error", function (err) {
     console.log("Error " + err);
 });
 
-var q = async.queue(function (data, callback) {
-    rClient.hset("raw", "data"+data.index, data.data);
-    //console.log(data);
-    callback();
-});
+
+fetcher ();
 
 for(var i=0; i < ports.length; i++){
 
@@ -41,15 +39,7 @@ for(var i=0; i < ports.length; i++){
 
     client.on("message", function (data, rinfo) {
         r++;
-       // console.log("Success:" + r);
-
-        q.push({index: r, data: data}, function (err) {
-            if(err){
-                console.log("Error queue: ", err);
-            }
-        });
-
-
+        FIFO.push(data,r);
         //console.log("Number process: ", q.length());
         //console.log("Server got: ".yellow + " IP: " +
         //rinfo.address + " - Port:" + rinfo.port);
@@ -57,3 +47,19 @@ for(var i=0; i < ports.length; i++){
 
     client.bind(ports[i]);
 }
+
+function fetcher () {
+    while (FIFO.length > 0)
+    {
+        var msg = FIFO.shift();
+        console.log("Message: ", msg);
+        process.nextTick(fetcher);
+    }
+    //setImmediate(fetcher); //make this function continuously run
+}
+
+var q = async.queue(function (data, callback) {
+    rClient.hset("raw", "data"+data.index, data.data);
+    //console.log(data);
+    callback();
+});
