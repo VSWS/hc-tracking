785d60827b269e2fa7731521bf1d9b9f62e28b29
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 14:49:22 +0700

    Update UDPefef

--- udp.js
+++ udp.js
@@ -1,25 +1,59 @@
-var d = require ('dequeue');
-var seatStateStore = require("./SeatStateStore");
+/**
+ * Created by tungtouch on 2/5/15.
+ */
+
+/*
+* Listen UDP from Device: 128.199.126.250
+* */
+
 var dgram = require("dgram");
-var FIFO = new d();
+var colors = require('colors');
+var d = require('dequeue')
+var async = require('async');
+var redis = require("redis");
+var rClient = redis.createClient();
+var fifo = new d();
+//var ports = [4000, 4001, 4002, 3333, 4003, 4004, 4005, 4006, 4007, 4008, 4009, 4010, 3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009, 3010, 3011];
+var ports = [4444];
+var r = 0;
+
+var client = dgram.createSocket('udp4');
+
+
+rClient.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+
 fetcher();
+for(var i=0; i < ports.length; i++){
 
-var udpserver = dgram.createSocket("udp4");
 
-udpserver.on("message",
-    function (msg, rinfo) {
-        console.log(FIFO);
-        FIFO.push(msg);
-    }
-);
+    console.log("Start listen port ",i,":", ports[i]);
 
-udpserver.bind(4444);
+    client.on("error", function (err) {
+        console.log("Server error:\n".red + err.stack);
+        client.close();
+    });
 
-function fetcher () {
-    while (FIFO.length > 0) {
-        var msg = FIFO.shift();
-        console.log(msg);
-        seatStateStore.parseMessage(msg);
+
+    client.on("message", function (msg, rinfo) {
+        //console.log("Message:", msg);
+        fifo.push(msg);
+
+    });
+
+    client.bind(ports[i]);
+
+
+}
+function fetcher() {
+
+    while (fifo.length > 0)
+    {
+        var msg = fifo.shift();
+        console.log("Message: ", msg);
         process.nextTick(fetcher);
     }
+    //setImmediate(fetcher); //make this function continuously run
 }
