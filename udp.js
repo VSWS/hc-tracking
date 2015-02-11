071aa9266819c05aada8240a91db02f142fe247a
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 15:56:52 +0700

    Update UDPefef

--- udp.js
+++ udp.js
@@ -28,13 +28,10 @@ var index = 0;
 function init() {
     while (FIFO.length > 0)
     {
-        console.log("INSIDE");
         index++;
         var msg = FIFO.shift();
         rClient.hset("raw", "data"+index, msg);
         //console.log("msg", msg);
         //process.nextTick(init);
     }
-    console.log("Outside");
-
 }
