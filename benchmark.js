9f9c26e134c3fbd1bcc4d68ef033344c22450c0b
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 12:32:02 +0700

    Update UDPefef

--- benchmark.js
+++ benchmark.js
@@ -39,7 +39,7 @@ if (cluster.isMaster) {
                 colors.yellow("Total Request: ", totalReqs), " | ",
                 colors.blue("Seconds: ", second++));
             numReqs = 0;
-            if(totalReqs == (max * numCluster)) {
+            if(totalReqs == 100000) {
                 flag = 1;
             }
         }, 1000);
