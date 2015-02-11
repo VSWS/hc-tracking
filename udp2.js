58b93a4337671c108f767351fd606b87089e4bc1
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 13:56:45 +0700

    Update UDPefef

--- udp2.js
+++ udp2.js
@@ -1,31 +1,51 @@
-/**
- * Created by tungtouch on 2/7/15.
- */
+// Let's create a DNS-proxy that proxies IPv4 udp-requests to googles IPv6 DNS-server 
+var proxy = require('udp-proxy'),
+    options = {
+        address: '0.0.0.0',
+        port: 4444,
+        ipv6: false,
+        localaddress: '0.0.0.0',
+        localport: 53535,
+        localipv6: false,
+        proxyaddress: '::0',
+        timeOutTime: 10000
+    };
 
-// Require the dgram module
-var dgram = require('dgram');
+// This is the function that creates the server, each connection is handled internally 
+var server = proxy.createServer(options);
 
-// Take in the host from command-line input
-var host = process.argv[2];
-// Take in port number (and parse as an integer)
-var port = parseInt(process.argv[3], 10);
-
-// Create the client
-client = dgram.createSocket('udp4');
-
-// Accept input  via standard input
-process.stdin.resume();
-
-// Listen for incoming standard input
-process.stdin.on('data', function (data) {
-    // Send all data to the client.
-    client.send(data, 0, data.length, port, host);
+// this should be obvious 
+server.on('listening', function (details) {
+    console.log('DNS - IPv4 to IPv6 proxy }>=<{ by: ok 2012');
+    console.log('udp-proxy-server ready on ' + details.server.family + '  ' + details.server.address + ':' + details.server.port);
+    console.log('traffic is forwarded to ' + details.target.family + '  ' + details.target.address + ':' + details.target.port);
 });
 
-// Listen for messages from client
-client.on('message', function (message) {
-    console.log("Client: " + message.toString());
+// 'bound' means the connection to server has been made and the proxying is in action 
+server.on('bound', function (details) {
+    console.log('proxy is bound to ' + details.route.address + ':' + details.route.port);
+    console.log('peer is bound to ' + details.peer.address + ':' + details.peer.port);
 });
 
-console.log("To send a message, " +
-"type now and press return.");
+// 'message' is emitted when the server gets a message 
+server.on('message', function (message, sender) {
+    console.log('message from ' + sender.address + ':' + sender.port);
+});
+
+// 'proxyMsg' is emitted when the bound socket gets a message and it's send back to the peer the socket was bound to 
+server.on('proxyMsg', function (message, sender) {
+    console.log('answer from ' + sender.address + ':' + sender.port);
+});
+
+// 'proxyClose' is emitted when the socket closes (from a timeout) without new messages 
+server.on('proxyClose', function (peer) {
+    console.log('disconnecting socket from ' + peer.address);
+});
+
+server.on('proxyError', function (err) {
+    console.log('ProxyError! ' + err);
+});
+
+server.on('error', function (err) {
+    console.log('Error! ' + err);
+});
