15e04692039672b0e497a8c1664c6ef605bf9771
tungtouch <tungnguyen@vsoft.vn>
2015-02-11 15:02:24 +0700

    Update UDPefef

--- udp.js
+++ udp.js
@@ -20,14 +20,13 @@ udpserver.on("message",
 udpserver.bind(4444);
 
 function fetcher () {
-    while (FIFO.length > 0) {
-
+    while (FIFO.length > 0)
+    {
         var msg = FIFO.shift();
-        console.log("Goi tin: ", msg);
-        //rClient.hset("raw", "data"+index, msg);
         seatStateStore.parseMessage(msg);
-        process.nextTick(fetcher);
+        console.log("mg:", msg);
     }
+    setImmediate(fetcher); //make this function continuously run
 }
 
 ///
