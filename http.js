73df59d7b3e2d66498ea1dc3c1d13f5631664595
tungtouch <tungnguyen@vsoft.vn>
2015-02-13 04:34:10 +0700

    Update UDPefef

--- http.js
+++ http.js
@@ -8,7 +8,7 @@ http.createServer(function(req, res) {
     i++;
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<p>Hello World</p>',i++);
-    console.log("Request server:", i++)
+    console.log("Request server:", i++, req)
     res.end();
 }).listen(7878);
 console.log("Server running: 7878");
---
+++ tcp-server2.js
@@ -0,0 +1,40 @@
+/**
+ * Created by tungtouch on 2/13/15.
+ */
+/**
+ * Created by tungtouch on 2/9/15.
+ */
+var net = require('net');
+var port = 7878;
+var index = 0;
+
+// Server IP: 128.199.126.250
+var server = net.createServer(function (socket) {
+
+    console.log("Client connected!");
+    // create connection to TCPe
+
+    //socket.pipe(socket);
+
+    socket.on('data', function (data) {
+        //socket.write("Server:", data);
+        //var result = data.toString();
+        console.log("Index: ", index++);
+
+    });
+
+    //socket.write("Welcome, i'm Server ! \r\n");
+
+    socket.on('error', function (err) {
+        console.log("Error server:", err.toString());
+    });
+
+    socket.on('end', function () {
+        console.log("Client disconnect server");
+    });
+
+});
+
+server.listen(port, function () {
+    console.log("Server Running Port: ", port);
+});
