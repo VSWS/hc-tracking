69cef23af2bc49b61ef967b09205dd6d5eb8347f
tungtouch <tungnguyen@vsoft.vn>
2015-02-13 09:47:38 +0700

    Update UDPefef

--- nga.js
+++ nga.js
@@ -6,9 +6,9 @@ var logdata = new Buffer("hehe this is new buffer");
 var client = dgram.createSocket('udp4', function(data){
     //console.log(logdata);	
     //console.log(new Buffer(data));
-    var ip = data.slice(6, 10).toString('hex');
+    var ip = data.slice(6, 10).toString();
     console.log(data);
-    console.log(data.toString('hex'));
+    console.log(data.toString());
     //console.log(ip);
     //console.log(new Buffer('29298000', 'hex'));
     //console.log("String data: "	 + data.toString('hex'));
--- udp-listenning.js
+++ udp-listenning.js
@@ -7,8 +7,57 @@ var colors = require('colors');
 var ports = [4000, 4001, 4002, 4003, 4004, 4005, 4006, 4007, 4008, 4009, 4010];
 
 var structer = {
-
+    'v1' : {
+        "30" : {
+            'des' : 'Tìm xem vị trí hiện tại của xe trong chỉ định',
+            'type' : 'server',
+            'res'  : '81',
+            'pro': 'udp/tcp'
+        },
+        "3D" : {
+            'des' : 'Tìm kiếm phiên bản tin của chiếc xe chỉ định',
+            'type' : 'server',
+            'res' : '2B',
+            'pro' : 'udp/tcp'
+        },
+        '28' : {
+            'des' : 'thu thập hình ảnh',
+            'type': 'server',
+            'res' : '85',
+            'pro' : 'udp/tcp'
+        },
+        '2A' : {
+            'des' : 'Chup anh lien tiep 10s',
+            'type': 'server',
+            'res' : '85',
+            'pro' : 'udp/tcp'
+        },
+        '21': {
+            'des' : 'Server gui xac nhan menh lenh',
+            'type': 'server',
+            'pro' : 'udp'
+        },
+        '80': {
+            'des': 'Du lieu vi tri',
+            'type': 'client',
+            'res' : '21',
+            'pro' : 'udp'
+        },
+        'B1': {
+            'des': 'Du lieu nhay',
+            'type': 'client',
+            'res': '21',
+            'pro': 'udp'
+        },
+        'E0': {
+            'des': 'Nguoi dang ky lai xe thoat',
+            'type': 'client',
+            'res' : '21',
+            'pro' : 'udp'
+        }
+    }
 }
+
 
 for(var i=1; i < ports.length; i++){
     console.log("Start listen port ",i,":",ports[i]);
